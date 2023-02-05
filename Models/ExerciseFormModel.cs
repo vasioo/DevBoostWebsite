85a12efc804c04018bc3a79c48207fe2a3e6725c
Vasil <vaskoorakov17@gmail.com>
2023-02-05 18:33:16 +0200

    fixed the main page and added a new add an exercise section

---
+++ Models/ExerciseFormModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DevBoost.Models
+{
+    public class ExerciseFormModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Input { get; set; }
+
+        [Required]
+        public string Output { get; set; }
+
+    }
+}
