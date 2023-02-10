c6b1b2bdc3dd54edd5438fc302e33330b77277f0
Vasil <vaskoorakov17@gmail.com>
2023-02-10 20:22:13 +0200

    added the database tables

---
+++ Data/Migrations/20230210174757_firstOne.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevBoost.Data.Migrations
+{
+    public partial class firstOne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
