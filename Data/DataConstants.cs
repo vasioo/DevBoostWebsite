1dbbffee10ca9eb1f4a6d201c3eab948a04a8ed6
Vasil <vaskoorakov17@gmail.com>
2023-02-12 17:37:24 +0200

    added new projects and classes where the project wil be distributed

---
+++ Data/DataConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace DevBoost.Data
+{
+    public class DataConstants
+    {
+        public class Adding
+        {
+            public const int TitleMaxLength = 50;
+            public const int TitleMinLength = 10;
+
+            public const int InputMaxLength = 150;
+            public const int InputMinLength = 0;
+
+            public const int DescriptionMaxLength = 500;
+            public const int DescriptionMinLength = 10;
+
+            public const int OutputMaxLength = 150;
+            public const int OutputMinLength = 0;
+        }
+    }
+}
---
+++ Models/TaskFormModel.cs
@@ -0,0 +1,34 @@
+﻿using DevBoost.Services;
+using System.ComponentModel.DataAnnotations;
+using static DevBoost.Data.DataConstants.Adding;
+
+namespace DevBoost.Models
+{
+    public class TaskFormModel
+    {
+        //TODO: add a mapping profiler 
+
+        [Required]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string? Title { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+        public string? Description { get; set; }
+
+        [Required]
+        [StringLength(InputMaxLength, MinimumLength = InputMinLength)]
+        public string? Input { get; set; }
+
+        [Required]
+        [StringLength(OutputMaxLength, MinimumLength = OutputMinLength)]
+        public string? Output { get; set; }
+
+
+        public IEnumerable<TaskCategoryServiceModel> Categories { get; set; }
+        = new List<TaskCategoryServiceModel>();
+    }
+}
